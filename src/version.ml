7d2d30d040a9fc85b8cb3393beda8d58e6e30a40
Valerio Bigiani <vbigiani@yahoo.it>
2009-10-15 21:27:19 +0200

    211.04

--- src/version.ml
+++ src/version.ml
@@ -6,7 +6,7 @@ diffs/src.version.ml.diff file, as the output of a diff -Bw -c -N command.
 
 It was originally taken from Westley Weimer's WeiDU 185. *)
 
-let version = "21103"
+let version = "21104"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
