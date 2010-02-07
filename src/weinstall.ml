72753304994a21fdd22f1b3ea341655eff8807c5
Valerio Bigiani <vbigiani@yahoo.it>
2010-02-07 22:57:01 +0100

    testing is good

--- src/weinstall.ml
+++ src/weinstall.ml
@@ -19,14 +19,17 @@ let main () =
     with _ -> Printf.sprintf "setup-%s.debug" Sys.argv.(1);
     in
     let we = Case_ins.weidu_executable in
-    let weidu_executable, fast = try
+    let fast = try
       let s = Sys.argv.(0) in
-      let fast = String.uppercase (Str.string_before (Filename.basename s) 4) = "FAST" in
+      String.uppercase (Str.string_before (Filename.basename s) 4) = "FAST"
+    with _ -> false in
+    let weidu_executable = try
+      let s = Sys.argv.(0) in
       let s = Str.string_after s (String.index s '-') in
       let s = Str.string_before s (String.rindex s '.') in
       let we = Str.string_before we (String.rindex we '.') in
-      we ^ s ^ ".exe", fast
-    with _ -> we, false in
+      we ^ s ^ ".exe"
+    with _ -> we in
     Buffer.add_string buff (Printf.sprintf "%s --tlkout dialog.tlk --ftlkout dialogf.tlk --log %s "
                               weidu_executable debug_where);
     let x = Sys.argv.(1) in
