102ae0e62b57fee81ece1ab6aef748cbf6206efc
Valerio Bigiani <vbigiani@yahoo.it>
2010-08-02 13:38:33 +0200

    219.00

--- src/version.ml
+++ src/version.ml
@@ -6,7 +6,7 @@
 
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
-let version = "21802"
+let version = "21900"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
@@ -220,4 +220,5 @@ let version = "21802"
 (* let comment = "Testing R Us. Also will always remain free beer *and* free speech" *)
 (* 216 let comment = "RERO, take two" *)
 (* 217 let comment = "Just because Miloch asked nicely" *)
-let comment = "People love monthly releases"
+(* 218 let comment = "People love monthly releases" *)
+let comment = "Anomaly detection (or something like that)"
