68933e4ca16a7854884693d521cec8e701dd47bc
Valerio Bigiani <vbigiani@yahoo.it>
2010-09-30 14:15:31 +0200

    two LABELs per component

--- src/tpstate.ml
+++ src/tpstate.ml
@@ -92,22 +92,11 @@ let get_last_module_index tp_file =
 
 let get_id_of_label tp_file label =
 	let ans = ref None in
-	let get_label i c =
-		let ans = ref "" in
-		List.iter (fun m ->
-			match m with
-			| TPM_Label s -> (match !ans with
-				| "" -> ans := s
-				| x -> failwith (Printf.sprintf "Component %d of [%s] has two labels" i tp_file.tp_filename)
-				)
-			| _ -> ()
-		) c.mod_flags;
-		!ans
-	in
+	let has_label c = List.mem (TPM_Label label) c.mod_flags in
 	for i = 0 to get_last_module_index tp_file do
 		try
 			let c = get_nth_module tp_file i false in
-			if get_label i c = label then begin match !ans with
+			if has_label c then begin match !ans with
 				| None -> ans := Some i
 				| Some j -> 
 					failwith (Printf.sprintf "Duplicate LABEL [%s] in tp2 file [%s] (components %d and %d)" label tp_file.tp_filename i j)
