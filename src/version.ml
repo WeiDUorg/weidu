b82551025973ca0f32c79e02523b9d5ed021ae73
Valerio Bigiani <vbigiani@yahoo.it>
2011-03-02 10:47:21 +0100

    228.02

--- src/version.ml
+++ src/version.ml
@@ -6,7 +6,7 @@
 
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
-let version = "22801"
+let version = "22802"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
