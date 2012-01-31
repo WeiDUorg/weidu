f2a77cdb78e9b82dfd4a5003a6c4f891739b6165
Valerio Bigiani <vbigiani@yahoo.it>
2012-01-31 22:06:29 +0100

    231.02

--- src/version.ml
+++ src/version.ml
@@ -7,7 +7,7 @@
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
 open BatteriesInit
-let version = "23101"
+let version = "23102"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
