c37451c16d7d1af255dce1f8fe420444b2462e9b
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2012-12-18 21:31:58 +0100

    231.06

--- src/version.ml
+++ src/version.ml
@@ -1,3 +1,6 @@
+(* This file has been edited by Fredrik Lindgren, a.k.a. Wisp,
+   starting from 18 December 2012 and WeiDU 232. *)
+
 (* Note added due to LGPL terms.
 
    This file was edited by Valerio Bigiani, AKA The Bigg, starting from
@@ -7,7 +10,7 @@
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
 open BatteriesInit
-let version = "23105"
+let version = "23106"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
