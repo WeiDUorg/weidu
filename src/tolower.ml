e71c7027dc9824de12f5fca6070ea61d7a26323f
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2013-08-21 18:30:27 +0200

    Reformat tolower.ml
    - Consistent indentation
    - Untabify
    - Delete trailing whitespace
    - Remove some unnecessary stuff

--- src/tolower.ml
+++ src/tolower.ml
@@ -1,3 +1,6 @@
+(* This file has been edited by Fredrik Lindgren, a.k.a. Wisp,
+   starting from 18 December 2012 and WeiDU 231.06. *)
+
 (*
  * Module to reduce all files in the base directory to lowercase
  * and other Linux-only boringness
@@ -11,21 +14,20 @@ open BatteriesInit
 exception Good_exit;;
 let askfor func mess =
   print_string mess;
-  try 
+  try
     while true do
       print_string "[Y]es or [N]o\n";
       let x = read_line () in
       match String.uppercase x with
       | "Y" ->
-	  func ();
-	  raise Good_exit;
+          func ();
+          raise Good_exit;
       | "N" ->
-	  raise Good_exit;
+          raise Good_exit;
       | _ ->
-	  ()
+          ()
     done
   with Good_exit -> ()
-;;
 
 (*
  * Turn all files to lowercase, recursively.
@@ -64,16 +66,15 @@ let rec find_and_lower cur_dir () =
         Hashtbl.add done_ht (String.lowercase element) true;
       end;
     done
-  with End_of_file -> Unix.closedir dh;
+  with End_of_file ->
+    Unix.closedir dh;
     List.iter (fun (x, do_del) ->
-	    find_and_lower x ();
-		if do_del then Unix.rmdir x
-	  ) !dirlist;
-;;
+      find_and_lower x ();
+      if do_del then Unix.rmdir x) !dirlist
 
 (*
  * Generate linux.ini from baldur.ini using /unix/style/paths rather than
- * W:\indows\style\paths 
+ * W:\indows\style\paths
  *)
 
 let get_wine_cfg () =
@@ -85,43 +86,40 @@ let get_wine_cfg () =
   Array.iter (fun this ->
     let thisdest = Unix.readlink (winepath ^ "/" ^ this) in
     let relative = Str.regexp "..?/" in
-    let thisdest = if Str.string_match relative thisdest 0 then (winepath ^ "/" ^ thisdest) else thisdest in
-    Hashtbl.add allpaths (String.uppercase this) thisdest;
-	     ) winelst ;
+    let thisdest = if Str.string_match relative thisdest 0 then
+      (winepath ^ "/" ^ thisdest)
+    else thisdest in
+    Hashtbl.add allpaths (String.uppercase this) thisdest) winelst ;
   (* Read all directory list in baldur.ini and create linux.ini from it *)
   let linuxini = open_out "linux.ini" in
   Array.iter (fun file ->
-	if (Filename.check_suffix file ".ini") then (
-	  let baldurini = open_in file in
-	  let cdregex = Str.regexp "[ \t]*[HC]D.:=" in
-	  try
-		while true do
-		  let line = input_line baldurini in
-		  if Str.string_match cdregex line 0 then begin
-		let split = Str.split (Str.regexp "[=;]") line in
-		List.iter (fun this ->
-		  let path = Str.string_before this 2 in
-		  let path = Hashtbl.find allpaths (String.uppercase path) in
-		  let otherpart = Str.string_after this 2 in
-		  let newpath = path ^ otherpart in
-		  let newpath = Str.global_replace (Str.regexp "\\\\") "/" newpath in
-							  Printf.fprintf linuxini "CD1:=%s\n%!" newpath;
-						   ) (List.tl split)
-		  end;
-		done
-			  with End_of_file -> ();
-				close_in baldurini;
-	)
-  ) (Sys.readdir ".");
-  close_out linuxini;
-;;
+    if (Filename.check_suffix file ".ini") then begin
+      let baldurini = open_in file in
+      let cdregex = Str.regexp "[ \t]*[HC]D.:=" in
+      try
+        while true do
+          let line = input_line baldurini in
+          if Str.string_match cdregex line 0 then begin
+            let split = Str.split (Str.regexp "[=;]") line in
+            List.iter (fun this ->
+              let path = Str.string_before this 2 in
+              let path = Hashtbl.find allpaths (String.uppercase path) in
+              let otherpart = Str.string_after this 2 in
+              let newpath = path ^ otherpart in
+              let newpath = Str.global_replace (Str.regexp "\\\\") "/" newpath in
+              Printf.fprintf linuxini "CD1:=%s\n%!" newpath) (List.tl split)
+          end;
+        done
+      with End_of_file -> ();
+        close_in baldurini
+    end) (Sys.readdir ".");
+  close_out linuxini
 
 (*
  * Ask the user which functions to run
  *)
 askfor (find_and_lower ".") "Do you want to lowercase everything?
-  (run if you extracted some mods since the last time you ran this utility)\n"
-;
+  (run if you extracted some mods since the last time you ran this utility)\n" ;
+
 askfor get_wine_cfg "Do you want to generate linux.ini from baldur.ini?
-(needed once per installation)\n"
-;
+(needed once per installation)\n" ;
