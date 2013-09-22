a86b59d68df00c45dc6f2a25c22264d3d3633a29
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2013-09-22 18:02:12 +0200

    Version 231.14

--- src/version.ml
+++ src/version.ml
@@ -10,7 +10,7 @@
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
 open BatteriesInit
-let version = "23113"
+let version = "23114"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
