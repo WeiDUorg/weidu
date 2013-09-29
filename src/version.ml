c4980b072c7c2f9c31afaad9633c00f5592aaa7f
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2013-09-29 17:29:03 +0200

    Version 232.00

--- src/version.ml
+++ src/version.ml
@@ -10,7 +10,7 @@
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
 open BatteriesInit
-let version = "23114"
+let version = "23200"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
@@ -237,4 +237,5 @@ let version = "23114"
 (* 228 let comment = "Procastination is the b" *)
 (* 229 let comment = "Braaaains for Paaaper" *)
 (* 230 let comment = "Unpaid overtime sux" *)
-let comment = "Magic tricks using Hashtables"
+(* 231 let comment = "Magic tricks using Hashtables" *)
+let comment = "22 months of wretchedness"
