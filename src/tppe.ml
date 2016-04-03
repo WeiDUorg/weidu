WeiDUorg/weidu

commit 3c78442150aa256a620bb6d3e3f4924dd5780562

GAME_IS et al. do not fail on unrecognised games

src/tppe.ml
@@ -455,7 +455,7 @@ let rec eval_pe buff game p =
         | "IWDEE"      -> iwdee
         | "EET"        -> eet
         | "SOD" -> sod && not eet
-        | _ -> failwith (Printf.sprintf "No rule to identify %s" (String.uppercase this))
+        | _ -> log_and_print "WARNING: No rule to identify %s" (String.uppercase this)
       ) game_list in
       if res then 1l else 0l;
   end
@@ -486,7 +486,7 @@ let rec eval_pe buff game p =
       | "IWD2" -> iwd2
       | "CA" -> ca
       | "SOD" -> sod
-      | _ -> failwith (Printf.sprintf "GAME_INCLUDES has no rule for %s" (String.uppercase game_set))) in
+      | _ -> log_and_print "WARNING: GAME_INCLUDES has no rule for %s" (String.uppercase game_set)) in
       eval_pe buff game (PE_GameIs((String.concat " " list), true)) ;
   end
 

commit e0ac76f632a9d9a9124cec3dd3e9e56e4b9237f5

Fix compile error introduced in 3c78442150aa256a620bb6d3e3f4924dd5780562

src/tppe.ml
@@ -455,7 +455,7 @@ let rec eval_pe buff game p =
         | "IWDEE"      -> iwdee
         | "EET"        -> eet
         | "SOD" -> sod && not eet
-        | _ -> log_and_print "WARNING: No rule to identify %s" (String.uppercase this)
+        | _ -> log_and_print "WARNING: No rule to identify %s" (String.uppercase this) ; false
       ) game_list in
       if res then 1l else 0l;
   end
@@ -486,7 +486,7 @@ let rec eval_pe buff game p =
       | "IWD2" -> iwd2
       | "CA" -> ca
       | "SOD" -> sod
-      | _ -> log_and_print "WARNING: GAME_INCLUDES has no rule for %s" (String.uppercase game_set)) in
+      | _ -> log_and_print "WARNING: GAME_INCLUDES has no rule for %s" (String.uppercase game_set) ; [(String.uppercase game_set)]) in
       eval_pe buff game (PE_GameIs((String.concat " " list), true)) ;
   end
 