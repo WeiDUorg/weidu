414b1dfa0d6d80b18bad7c2d30d691225a68f67f
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2017-06-24 13:55:44 +0200

    Version 242.00

--- src/version.ml
+++ src/version.ml
@@ -10,7 +10,7 @@
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
 open BatteriesInit
-let version = "24100"
+let version = "24200"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
@@ -247,4 +247,5 @@ let version = "24100"
 (* 238 let comment = "d41d8cd98f00b204e9800998ecf8427e" *)
 (* 239 let comment = "d41d8cd98f00b204e9800998ecf8427e" *)
 (* 240 let comment = "d41d8cd98f00b204e9800998ecf8427e" *)
-let comment = "Heralding the second coming of GUI"
+(* 241 let comment = "Heralding the second coming of GUI" *)
+let comment = "No comment"
