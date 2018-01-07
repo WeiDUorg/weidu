22aa6ebd84cdb30810d419f42e0d1f07fc576b0d
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2018-01-07 21:47:45 +0100

    Version 243.00

--- src/version.ml
+++ src/version.ml
@@ -10,7 +10,7 @@
    It was originally taken from Westley Weimer's WeiDU 185. *)
 
 open BatteriesInit
-let version = "24202"
+let version = "24300"
 
 (* Historical Comments: *)
 (* 7 let comment = "Underdark Army Knife" *)
@@ -248,4 +248,5 @@ let version = "24202"
 (* 239 let comment = "d41d8cd98f00b204e9800998ecf8427e" *)
 (* 240 let comment = "d41d8cd98f00b204e9800998ecf8427e" *)
 (* 241 let comment = "Heralding the second coming of GUI" *)
-let comment = "No comment"
+(* 242 let comment = "No comment" *)
+let comment = "243 is not a prime number"
