WeiDUorg/weidu

commit de37cafef2ffe04e30c2fefe3fc3577642b0ca82

Autoupdate finds a way around inexecutable setup-mymods

src/arch_legacy_osx.ml
@@ -72,6 +72,7 @@ let is_weidu_executable f =
   with _ -> false
 
 let get_version f =
+  ignore (Unix.access f [ X_OK ]) ;
   let exec = Printf.sprintf "./%s --exit" f in
   Printf.printf "{%s} queried%!" f;
   let ic,oc,ec = Unix.open_process_full exec (Unix.environment()) in

src/arch_mingw.ml
@@ -76,12 +76,14 @@ let is_weidu_executable f =
   Str.string_match (Str.regexp_case_fold "setup-.*\.exe$") f 0
 
 let get_version f =
+  ignore (Unix.access f [ X_OK ]) ;
   let newstdin, newstdin' = Unix.pipe () in
   let newstdout, newstdout' = Unix.pipe () in
   let newstderr, newstderr' = Unix.pipe () in
   let pid = create_process_env
       f [| "WeiDU-Backup" ; "--game bar" |] [| |] newstdin newstdout' newstderr'
   in
+  if pid < 0 then failwith "invalid pid" ;
   Printf.printf "{%s} Queried (pid = %d)%!" f pid ;
   let ic = Unix.in_channel_of_descr newstdout in
   let line = input_line ic in

src/arch_osx.ml
@@ -61,6 +61,7 @@ let is_weidu_executable f =
   with _ -> false
 
 let get_version f =
+  ignore (Unix.access f [ X_OK ]) ;
   let exec = Printf.sprintf "./%s --exit" f in
   Printf.printf "{%s} queried%!" f;
   let ic,oc,ec = Unix.open_process_full exec (Unix.environment()) in

src/arch_unix.ml
@@ -59,12 +59,14 @@ let is_weidu_executable f =
   with _ -> false
 
 let get_version f =
+  ignore (Unix.access f [ X_OK ]) ;
   let newstdin, newstdin' = Unix.pipe () in
   let newstdout, newstdout' = Unix.pipe () in
   let newstderr, newstderr' = Unix.pipe () in
   let pid = create_process_env
       f [| "WeiDU-Backup" ; "--game bar" |] [| |] newstdin newstdout' newstderr'
   in
+  if pid < 0 then failwith "invalid pid" ;
   Printf.printf "{%s} Queried (pid = %d)%!" f pid ;
   let ic = Unix.in_channel_of_descr newstdout in
   let line = input_line ic in

src/autoupdate.ml
@@ -31,7 +31,10 @@ let get_version_list () =
             else begin
               let version = try
                 Arch.get_version f
-              with _ -> -1
+              with _ ->
+                Printf.printf
+                  "{%s} could not get version; let's call it...%!" f ;
+                -1
               in
               log_and_print " version = %d\n" version ;
               Hashtbl.add digest_ht f_digest version ;

commit 2a8bd0d42e9c8526c3f3af80d2456f1a39ba31fa

Fix scope error
Introduced in de37cafef2ffe04e30c2fefe3fc3577642b0ca82

src/arch_legacy_osx.ml
@@ -72,7 +72,7 @@ let is_weidu_executable f =
   with _ -> false
 
 let get_version f =
-  ignore (Unix.access f [ X_OK ]) ;
+  ignore (Unix.access f [ Unix.X_OK ]) ;
   let exec = Printf.sprintf "./%s --exit" f in
   Printf.printf "{%s} queried%!" f;
   let ic,oc,ec = Unix.open_process_full exec (Unix.environment()) in

src/arch_mingw.ml
@@ -76,7 +76,7 @@ let is_weidu_executable f =
   Str.string_match (Str.regexp_case_fold "setup-.*\.exe$") f 0
 
 let get_version f =
-  ignore (Unix.access f [ X_OK ]) ;
+  ignore (Unix.access f [ Unix.X_OK ]) ;
   let newstdin, newstdin' = Unix.pipe () in
   let newstdout, newstdout' = Unix.pipe () in
   let newstderr, newstderr' = Unix.pipe () in

src/arch_osx.ml
@@ -61,7 +61,7 @@ let is_weidu_executable f =
   with _ -> false
 
 let get_version f =
-  ignore (Unix.access f [ X_OK ]) ;
+  ignore (Unix.access f [ Unix.X_OK ]) ;
   let exec = Printf.sprintf "./%s --exit" f in
   Printf.printf "{%s} queried%!" f;
   let ic,oc,ec = Unix.open_process_full exec (Unix.environment()) in

src/arch_unix.ml
@@ -59,7 +59,7 @@ let is_weidu_executable f =
   with _ -> false
 
 let get_version f =
-  ignore (Unix.access f [ X_OK ]) ;
+  ignore (Unix.access f [ Unix.X_OK ]) ;
   let newstdin, newstdin' = Unix.pipe () in
   let newstdout, newstdout' = Unix.pipe () in
   let newstderr, newstderr' = Unix.pipe () in