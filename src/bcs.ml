WeiDUorg/weidu

commit a4048b11e6904f39ea1f956a36018e5d9a6e8e5c

Try to load movie files with wbm ext on EE-type games
Do not try to use mve ext

src/bcs.ml
@@ -347,7 +347,8 @@ let check_file_presence_action string script_style ids game first_or_second_stri
         -> [ "2da" ]
 
       | 167l (* StartMovie *)
-        -> [ "mve" ]
+        -> if (Load.eep) then
+          [ "wbm" ] else [ "mve" ]
 
       | 272l (* CreateVisualEffectPoint *)
       | 273l (* CreateVisualEffectObject *)

src/load.ml
@@ -278,6 +278,9 @@ let enhanced_edition_p game =
   | PSTEE -> true
   | GENERIC -> false)
 
+let eep =
+  enhanced_edition_p (the_game ())
+
 let load_dialog_pair path dpath dfpath =
   let dialog, dialog_path = fake_load_dialog path dpath in
   let dialogf, dialogf_path = fake_load_dialogf path dfpath in

commit 27c76103432976e67441988ade678708c7f3d0bf

Fix regression
Introduced in a4048b11e6904f39ea1f956a36018e5d9a6e8e5c

Code would try to run Load.the_game while defining symbols, waaaay
before any game had had any chance to load

src/bcs.ml
@@ -347,7 +347,7 @@ let check_file_presence_action string script_style ids game first_or_second_stri
         -> [ "2da" ]
 
       | 167l (* StartMovie *)
-        -> if (Load.eep) then
+        -> if (Load.eep ()) then
           [ "wbm" ] else [ "mve" ]
 
       | 272l (* CreateVisualEffectPoint *)

src/load.ml
@@ -278,7 +278,7 @@ let enhanced_edition_p game =
   | PSTEE -> true
   | GENERIC -> false)
 
-let eep =
+let eep () =
   enhanced_edition_p (the_game ())
 
 let load_dialog_pair path dpath dfpath =