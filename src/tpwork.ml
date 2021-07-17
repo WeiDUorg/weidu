WeiDUorg/weidu

commit 092a8d46825f78c9b5001f11d658876d845b58ce

Fix cosmetic logic error when interactively asking about subcomponents
Subcomponent predicates are true by default and display code was using
this in a disjunction to effectively override component requirements,
resulting in the interactive installer asking about components that
could not be installed

src/tpwork.ml
@@ -1112,8 +1112,8 @@ let rec handle_tp game this_tp2_filename tp =
                 handle_letter tp "U" can_uninstall temp_uninst package_name m finished i ;
             | Some(ts) when ts = subcomp ->
                 is_forced := !is_forced || (subcomp_forced m);
-                  at_least_one_OK := !at_least_one_OK || (subcomp_predicate m) ||
-                  not (fails_requirements tp m)
+                  at_least_one_OK := (!at_least_one_OK ||
+                  not (fails_requirements tp m)) && (subcomp_predicate m)
                 | _ -> ()
             with Not_found -> ()
           done ;

commit 832cb1f9dbc90fb0b4b1a4cbf1c9e7139b64afbc

Fix regression involving subcomponent predicates
Regression was introduced in 092a8d46825f78c9b5001f11d658876d845b58ce

If the last subcomponent had a false subcomponent predicate, the
interactive installer would skip all subcomponents, rather than just
those with failing predicates.

src/tpwork.ml
@@ -1131,8 +1131,9 @@ let rec handle_tp game this_tp2_filename tp =
                 handle_letter tp "U" can_uninstall temp_uninst package_name m finished i ;
             | Some(ts) when ts = subcomp ->
                 is_forced := !is_forced || (subcomp_forced m);
-                  at_least_one_OK := (!at_least_one_OK ||
-                  not (fails_requirements tp m)) && (subcomp_predicate m)
+                  let this_one_OK = not (fails_requirements tp m) &&
+                    (subcomp_predicate m) in
+                  at_least_one_OK := !at_least_one_OK || this_one_OK ;
                 | _ -> ()
             with Not_found -> ()
           done ;