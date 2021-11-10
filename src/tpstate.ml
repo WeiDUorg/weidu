e8d36d1a793ca638809acd8b86ad3df60ab0422b
Fredrik Lindgren <lindgren.fredrik@gmail.com>
2021-11-10 19:17:29 +0100

    Determination of MOD_FOLDER from TP2 path is more robust
    Unexpected inputs like an absolute TP2 path will not lead to comical
    results

--- src/tpstate.ml
+++ src/tpstate.ml
@@ -25,9 +25,19 @@ let rec get_menu_style fl = match fl with
 | hd :: tl -> get_menu_style tl
 
 let mod_folder tp =
-  (match Var.get_mod_folder tp.tp_filename with
-  | Some s -> s
-  | None -> (match Var.get_mod_folder tp.backup with
+  let parts = List.rev
+      (String.split_on_char '/'
+         (Str.global_replace
+            (Str.regexp "\\\\") "/" tp.tp_filename)) in
+  (match parts with
+  | file :: dir :: _ when
+      (String.equal
+         (String.lowercase
+            (Case_ins.filename_chop_extension
+               (Util.tp2_name
+                  (Case_ins.filename_basename file))))
+         (String.lowercase dir)) -> dir
+  | _ -> (match Var.get_mod_folder tp.backup with
     | Some s -> s
     | None -> tp.backup))
 
