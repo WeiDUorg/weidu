70f9f419b2d2e483799b082ac051bef39a2321da
dark0dave <dark0dave@mykolab.com>
2025-11-18 19:57:21 +0000

    fix(help): Better printing for modder options
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- src/main.ml
+++ src/main.ml
@@ -1620,14 +1620,14 @@ let main () =
     "--tlkcmp-use-strings", Myarg.Set tlkcmp_strings, "\tmodifies --tlkcmp behavior";
 
     "", Myarg.Unit (fun a -> a), "\nModder Options:\n" ;
-    "SETUP_TRA", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
-    "AREA_VARIABLES", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
-    "MISSING_EXTERN", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
-    "MISSING_RESREF", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
-    "ICT2_ACTIONS", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
-    "MISSING_EVAL", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "SETUP_TRA       ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "AREA_VARIABLES  ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "MISSING_EXTERN  ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "MISSING_RESREF  ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "ICT2_ACTIONS    ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "MISSING_EVAL    ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
     "OVERWRITING_FILE", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
-    "FUN_ARGS", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
+    "FUN_ARGS        ", Myarg.Unit (fun a -> a), "\tCan be set to NONE, WARN, FAIL";
 
     "", Myarg.Unit (fun a -> a), "\nLog Options:\n" ;
 
