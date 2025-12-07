98e7c1c56fafe40d09c5ece034384b240c5c5dc7
dark0dave <dark0dave@mykolab.com>
2025-12-07 17:46:06 +0000

    feat(strings): Remove depricated string functions or at least make a start
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- src/tpstate.ml
+++ src/tpstate.ml
@@ -32,11 +32,11 @@ let mod_folder tp =
   (match parts with
   | file :: dir :: _ when
       (String.equal
-         (String.lowercase
+         (String.lowercase_ascii
             (Case_ins.filename_chop_extension
                (Util.tp2_name
                   (Case_ins.filename_basename file))))
-         (String.lowercase dir)) -> dir
+         (String.lowercase_ascii dir)) -> dir
   | _ -> (match Var.get_mod_folder tp.backup with
     | Some s -> s
     | None -> tp.backup))
@@ -217,10 +217,10 @@ let get_component_list tp_file =
  * Evaluate a TP2 Patch Expression
  ************************************************************************)
 let log_match a b =
-  let a = String.uppercase a in
-  let b = String.uppercase b in
-  Str.global_replace (Str.regexp "^SETUP-") "" (Case_ins.filename_basename (String.uppercase a)) =
-  Str.global_replace (Str.regexp "^SETUP-") "" (Case_ins.filename_basename (String.uppercase b))
+  let a = String.uppercase_ascii a in
+  let b = String.uppercase_ascii b in
+  Str.global_replace (Str.regexp "^SETUP-") "" (Case_ins.filename_basename (String.uppercase_ascii a)) =
+  Str.global_replace (Str.regexp "^SETUP-") "" (Case_ins.filename_basename (String.uppercase_ascii b))
 
 let any_installed tp2 =
   let rec is_installed lst = match lst with
@@ -356,11 +356,11 @@ let sprintf_log game handle_tp2_filename handle_tra_filename get_tra_list_filena
         let component_name = Str.global_replace newline_regexp " " component_name in
         let subcomponent_name = Str.global_replace newline_regexp " " subcomponent_name in
         Printf.sprintf "~%s~ #%d #%d // %s%s%s\n"
-          (String.uppercase a) b c subcomponent_name component_name version
+          (String.uppercase_ascii a) b c subcomponent_name component_name version
       end
       else begin
         Printf.sprintf "~%s~ #%d #%d\n"
-          (String.uppercase a) b c
+          (String.uppercase_ascii a) b c
       end
     in
     match d with
